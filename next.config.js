49f0b8fa941f6501385987c15833fefd04ed8a0e
hacksecureofficial <hacksecure.info@gmail.com>
2025-02-24 16:57:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  "scripts": {
-  "dev": "next dev",
-  "build": "next build",
-  "start": "next start"
-}
-
+  // Enable React's Strict Mode for highlighting potential problems
   reactStrictMode: true,
+
+  // Configure allowed external image domains
   images: {
-    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"], // Added external image domain
+    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
   },
+
+  // Environment variables accessible in the application
   env: {
     JWT_SECRET: process.env.JWT_SECRET,
     SMTP_FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
@@ -19,6 +18,8 @@ const nextConfig = {
     SMTP_PASS: process.env.SMTP_PASS,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
+
+  // Rewrites to handle server-side API routing
   async rewrites() {
     return [
       {
